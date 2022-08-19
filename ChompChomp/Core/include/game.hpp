dc7b0ba3dc2f5db96c86b1f63d517d1adf08136b
Tello- <tellovisionstudio@gmail.com>
2022-08-18 22:52:43 -0700

    M and P zoom debug commands added

--- ChompChomp/Core/include/game.hpp
+++ ChompChomp/Core/include/game.hpp
@@ -1,9 +1,14 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <format>
 #include "SFML/Graphics.hpp"
 #include "../../Core/include/BasicWindow.h"
 
 namespace game
 {
+
+
 	enum GameState{OVER_WORLD, MINI_GAME};
 	enum GameStateTransition{FISH_ON, WIN_LOSE};
 
@@ -15,9 +20,16 @@ namespace game
 
 
 	private:
-		void Input();
+
+
+		void InitOverWorld();
+		void InitMiniGame();
+
+		void Input(std::vector<sf::Event>& events);
 		void Update();
 		void Render();
+
+		void PollEvents(std::vector<sf::Event>& eventVec);
 
 		void FishOn() 
 		{ 
@@ -29,8 +41,43 @@ namespace game
 		}
 
 
+
+		void OnClose(sf::Event& event);
+		void OnResized(sf::Event& event);
+		void OnLostFocus(sf::Event& event);
+		void OnGainedFocus(sf::Event& event);
+		void OnKeyPressed(sf::Event& event);
+		void OnKeyReleased(sf::Event& event);
+		void OnMouseButtonPressed(sf::Event& event);
+		void OnMouseButtonReleased(sf::Event& event);
+		void OnMouseMoved(sf::Event& event);
+		void OnMouseEntered(sf::Event& event);
+		void OnMouseLeft(sf::Event& event);
+
+		void OnOverWorld_MousePressed(sf::Event& event);
+		void OnOverWorld_MouseReleased(sf::Event& event);
+		
+		void OnMiniGame_MousePressed(sf::Event& event);
+		void OnMiniGame_MouseReleased(sf::Event& event);
+		
+		enum OverworldQuadrant { TL, TR, BL, BR };
+		bool is_TL_Active{ false };
+		bool is_TR_Active{ false };
+		bool is_BL_Active{ false };
+		bool is_BR_Active{ false };
+
+		std::vector<sf::FloatRect> overworldQuadrantVec;
+
 		GameState currState{ OVER_WORLD };
 		core::BasicWindow window;
+		sf::Texture pondTexture;
+		sf::Sprite pondSprite;
+		sf::Vector2f POND_SPRITE_SCALE{ 2.5f, 2.5f };
+
+		std::vector<sf::Sprite> overworld_drawables;
+		std::vector<sf::Sprite> minigame_drawables;
+
+		bool isImagesLoaded{ false };
 
 	};
 }
--- ChompChomp/Core/source/game.cpp
+++ ChompChomp/Core/source/game.cpp
@@ -1,4 +1,5 @@
 #include "../include/game.hpp"
+#include "../include/ImageFileReader.hpp"
 
 game::Game::Game() : window(500, 500,"Chomp Chomp")
 {
@@ -7,46 +8,248 @@ game::Game::Game() : window(500, 500,"Chomp Chomp")
 
 void game::Game::Run()
 {
+     InitOverWorld();  
+
+
+     
      while (window.IsOpen())
      {
+          std::vector<sf::Event> events;
+          PollEvents(events);
+          Input(events);
           Update();
           Render();
      }
 }
 
-void game::Game::Input()
+void game::Game::InitOverWorld()
 {
+     if (!isImagesLoaded)
+     {
+          core::ImageFileReader imgReader;
+
+          if (!imgReader.ReadImageFile("pond.png", pondTexture))
+          {
+
+          }
+     }
+     pondSprite.setTexture(pondTexture);
+     pondSprite.setScale(POND_SPRITE_SCALE);
+     pondSprite.setPosition(window.GetSize().x / 2.0f - pondSprite.getGlobalBounds().width/ 2.0f, window.GetSize().y / 2.0f - pondSprite.getGlobalBounds().height / 2.0f);
+
+     overworld_drawables.push_back(pondSprite);
+
+}
+
+void game::Game::InitMiniGame()
+{
+}
+
+void game::Game::Input(std::vector<sf::Event>& events)
+{
+     for (sf::Event i : events)
+     {
+          switch (i.type)
+          {
+          case sf::Event::EventType::Closed:
+               OnClose(i);
+               break;
+          case sf::Event::EventType::Resized:
+               OnResized(i);
+               break;
+          case sf::Event::EventType::LostFocus:
+               OnLostFocus(i);
+               break;
+          case sf::Event::EventType::GainedFocus:
+               OnGainedFocus(i);
+               break;
+          case sf::Event::EventType::KeyPressed:
+               OnKeyPressed(i);
+               break;
+          case sf::Event::EventType::KeyReleased:
+               OnKeyReleased(i);
+               break;
+          case sf::Event::EventType::MouseButtonPressed:
+               OnMouseButtonPressed(i);
+               break;
+          case sf::Event::EventType::MouseButtonReleased:
+               OnMouseButtonReleased(i);
+               break;
+          case sf::Event::EventType::MouseMoved:
+               OnMouseMoved(i);
+               break;
+          case sf::Event::EventType::MouseEntered:
+               OnMouseEntered(i);
+               break;
+          case sf::Event::EventType::MouseLeft:
+               OnMouseLeft(i);
+               break;
+          default:
+               break;
+          }
+     }
 }
 
 void game::Game::Update()
 {
-     sf::Event event;
-     while (window.PollEvent(event))
+     if (pondSprite.getScale().x != POND_SPRITE_SCALE.x || pondSprite.getScale().y != POND_SPRITE_SCALE.y)
      {
-         if (event.type == sf::Event::Closed)
-               window.Close();
-         /*if (event.type == sf::Event::MouseButtonPressed)
-         {
-
-               HandleMouseButtonPressed(win);
-
-               if (mySprite.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(win).x, sf::Mouse::getPosition(win).y)))
-               {
-
-                    focusBorder.SetActive(true);
-               }
-         }
-          if (event.type == sf::Event::MouseButtonReleased)
-          {
-               HandleMouseButtonReleased(win);
-
-               if (focusBorder.IsActive()) focusBorder.SetActive(false);
-          }*/
+          pondSprite.setScale(POND_SPRITE_SCALE);
+          pondSprite.setPosition(window.GetSize().x / 2.0f - pondSprite.getGlobalBounds().width / 2.0f, window.GetSize().y / 2.0f - pondSprite.getGlobalBounds().height / 2.0f);
+          std::cout << std::format("Pond Sprite Scale Set to {}, {}", POND_SPRITE_SCALE.x, POND_SPRITE_SCALE.y) << std::endl;
      }
+     
 }
 
 void game::Game::Render()
 {
      window.Clear();
+     window.Draw(pondSprite);
+
+     if (currState == GameState::MINI_GAME)
+     {
+          
+     }
      window.Display();
+}
+
+void game::Game::PollEvents(std::vector<sf::Event>& eventVec)
+{
+     sf::Event event;
+     while (window.PollEvent(event))
+     {
+          eventVec.push_back(event);
+     }
+}
+
+void game::Game::OnClose(sf::Event& event)
+{
+     window.Close();
+}
+
+void game::Game::OnResized(sf::Event& event)
+{
+
+}
+
+void game::Game::OnLostFocus(sf::Event& event)
+{
+}
+
+void game::Game::OnGainedFocus(sf::Event& event)
+{
+}
+
+void game::Game::OnKeyPressed(sf::Event& event)
+{
+}
+
+void game::Game::OnKeyReleased(sf::Event& event)
+{
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          if (event.key.code == sf::Keyboard::Key::P)
+          {
+               POND_SPRITE_SCALE.x += 0.50f;
+               POND_SPRITE_SCALE.y+= 0.50f;
+               sf::Vector2f newScale{ POND_SPRITE_SCALE};
+               //pondSprite.setScale(newScale);
+          }
+          if (event.key.code == sf::Keyboard::Key::M)
+          {
+               POND_SPRITE_SCALE.x -= 0.50f;
+               POND_SPRITE_SCALE.y -= 0.50f;
+               //pondSprite.setScale(newScale);
+               //pondSprite.setScale(newScale);
+          }
+               
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnMouseButtonPressed(sf::Event& event)
+{ 
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          OnOverWorld_MousePressed(event);
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnMouseButtonReleased(sf::Event& event)
+{
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnMouseMoved(sf::Event& event)
+{
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnMouseEntered(sf::Event& event)
+{
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnMouseLeft(sf::Event& event)
+{
+     switch (currState)
+     {
+     case game::OVER_WORLD:
+          break;
+     case game::MINI_GAME:
+          break;
+     default:
+          break;
+     }
+}
+
+void game::Game::OnOverWorld_MousePressed(sf::Event& event)
+{
+}
+
+void game::Game::OnOverWorld_MouseReleased(sf::Event& event)
+{
+}
+
+void game::Game::OnMiniGame_MousePressed(sf::Event& event)
+{
+}
+
+void game::Game::OnMiniGame_MouseReleased(sf::Event& event)
+{
 }
